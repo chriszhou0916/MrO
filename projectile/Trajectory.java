f1288171ed102202b91de04fd48b1c39dfb7312c
Chris Zhou <chriszhou0916@hotmail.com>
2015-05-18 18:18:54 -0700

    啊

--- projectile/Trajectory.java
+++ projectile/Trajectory.java
@@ -5,7 +5,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class FlyingThing
+public class Trajectory
 {
     // instance variables - replace the example below with your own
     private int x;
