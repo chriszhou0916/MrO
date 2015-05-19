d9a56e471cdee6a32c0ced88534cc4c9c2e2ee3a
Chris Zhou <chriszhou0916@hotmail.com>
2015-05-18 18:23:54 -0700

    test

--- projectile/Trajectory.java
+++ projectile/Trajectory.java
@@ -7,27 +7,10 @@
  */
 public class Trajectory
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class FlyingThing
-     */
-    public FlyingThing()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    private double currX,currY,initialX,initialY;
+    private double currVX,currVY,initialVX,initialVY;
+    private double currAX,currAY,gravity;
+    private double initialAngle,initialV;
+    private double mass,diameter;
+    private String item;
 }
