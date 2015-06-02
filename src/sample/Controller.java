3f68cb463648193f877dd59c81a76901a6058910
YOUR NAME <chriszhou@Chriss-MacBook-Pro.local>
2015-06-02 13:53:12 -0700

    ok

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,7 @@ import javafx.beans.binding.Bindings;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point3D;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Node;
@@ -20,6 +21,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 import javafx.scene.image.*;
@@ -53,6 +55,8 @@ public class Controller implements Initializable{
     @FXML
     private TextField diameterField;
     @FXML
+    private TextField gravityField;
+    @FXML
     private BorderPane border;
     @FXML
     private ImageView target;
@@ -62,13 +66,16 @@ public class Controller implements Initializable{
     private ImageView canonViewer;
     @FXML
     private Pane centralPane;
+    @FXML
+    private Circle canonBackCircle;
+    @FXML
+    private Circle canonBarrel;
 
     private Canvas bg,trajectoryLayer,projectileLayer;
     private AnimationTimer timer;
     private GraphicsContext gc,gcTraj;
     private Model circle;
     private boolean isAnimating;
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         //initialize itemList
@@ -78,19 +85,18 @@ public class Controller implements Initializable{
         Image img = new Image("file:target-round.png");
         Image canon = new Image("file:canon.png");
         canonViewer.setImage(canon);
-        makeDrag(canonViewer);
+        makeDrag2(canonViewer,canonBackCircle);
         target.setImage(img);
         makeDrag(target);
         angleField.setOnKeyReleased(e -> {
             if(!isDouble(angleField))
-
             e.consume();
-            else{
+            else
                 canonViewer.setRotate(-Double.parseDouble(angleField.getText()));
-            }
-
         });
-         //centralPane.getChildren().add(makeDraggable(target));
+        makeRotate2(canonBackCircle, canonViewer);
+
+
     }
 
 
@@ -106,26 +112,23 @@ public class Controller implements Initializable{
 
     }
     public void fireButtonClicked() {
+        System.out.println(canonBarrel.getLayoutX() + canonBarrel.getTranslateX() + ", " + canonBarrel.getLayoutY());
+        String selected = itemList.getSelectionModel().getSelectedItem();
+        if (selected == null) {
+            ErrorMessage.showMessage("please select a projectile");
+            return;
+        }
         if(isAnimating){
             ErrorMessage.showMessage("one animation is running already\nplease click erase");
             return;}
-        isAnimating = true;
-        if (!isDouble(angleField) || !isDouble(initialVField))
+        if (!isDouble(angleField) || !isDouble(initialVField) || !isDouble(gravityField))
             return;
-        circle = new Model(canonViewer.getLayoutX() + 190, canonViewer.getLayoutY(), Double.parseDouble(angleField.getText()), Double.parseDouble(initialVField.getText()));
-        circle.setG(5);
-        circle.initialize();
-        circle.fire();
-//        Circle shape = new Circle(10,10,10);
-//        shape.setFill(Color.RED);
-//        shape.setLayoutY(circle.getY());
-//        shape.setLayoutX(circle.getX());
+        circle = new Model(canonBarrel.getTranslateX(), canonBarrel.getCenterY(), Double.parseDouble(angleField.getText()), Double.parseDouble(initialVField.getText()));
 
+        circle.setG(Double.parseDouble(gravityField.getText()));
+        isAnimating = true;
         trajectoryLayer = new Canvas(centralPane.getWidth(), centralPane.getHeight());
         projectileLayer = new Canvas(centralPane.getWidth(), centralPane.getHeight());
-        canonViewer.setRotate(-Double.parseDouble(angleField.getText()));
-
-
         gc = projectileLayer.getGraphicsContext2D();
         gcTraj = trajectoryLayer.getGraphicsContext2D();
         gc.setFill(Color.RED);
@@ -138,11 +141,7 @@ public class Controller implements Initializable{
 //        Timeline timeline = new Timeline();
 //        timeline.setCycleCount(Timeline.INDEFINITE);
 
-        String selected = itemList.getSelectionModel().getSelectedItem();
-        if (selected == null) {
-            ErrorMessage.showMessage("please select a projectile");
-            return;
-        }
+
         timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -177,7 +176,7 @@ public class Controller implements Initializable{
 
     public void updateFields()
     {
-        heightField.setText(String.valueOf(circle.getX()));
+        heightField.setText(String.valueOf(circle.getAltitude()));
     }
 
 
@@ -189,23 +188,58 @@ public class Controller implements Initializable{
                 dragDelta.y = n.getLayoutY() - mouseEvent.getSceneY();
                 n.setCursor(Cursor.MOVE);
             });
-
-        n.setOnMouseReleased(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
-                n.setCursor(Cursor.HAND);
-            }
-        });
-        n.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
+        n.setOnMouseReleased(e -> n.setCursor(Cursor.HAND));
+        n.setOnMouseDragged(mouseEvent-> {
                 n.setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
                 n.setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
-            }
+            });
+        n.setOnMouseEntered(e -> n.setCursor(Cursor.HAND));
+    }
+
+    private void makeDrag2(Node n,Node m){
+        final Delta dragDelta = new Delta();
+        final Delta dragDelta2 = new Delta();
+        n.setOnMousePressed(mouseEvent-> {
+            // record a delta distance for the drag and drop operation.
+            dragDelta.x = n.getLayoutX() - mouseEvent.getSceneX();
+            dragDelta.y = n.getLayoutY() - mouseEvent.getSceneY();
+            dragDelta2.x = m.getLayoutX() - mouseEvent.getSceneX();
+            dragDelta2.y = m.getLayoutY() - mouseEvent.getSceneY();
+            n.setCursor(Cursor.MOVE);
         });
-        n.setOnMouseEntered(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent mouseEvent) {
-                n.setCursor(Cursor.HAND);
-            }
+        n.setOnMouseReleased(e -> n.setCursor(Cursor.HAND));
+        n.setOnMouseDragged(mouseEvent-> {
+            n.setLayoutX(mouseEvent.getSceneX() + dragDelta.x);
+            n.setLayoutY(mouseEvent.getSceneY() + dragDelta.y);
+            m.setLayoutX(mouseEvent.getSceneX() + dragDelta2.x);
+            m.setLayoutY(mouseEvent.getSceneY() + dragDelta2.y);
         });
+        n.setOnMouseEntered(e -> n.setCursor(Cursor.HAND));
+    }
+
+    private void makeRotate2(Node n,Node m){
+        final Delta dragDelta = new Delta();
+        n.setOnMousePressed(mouseEvent-> {
+            // record a delta distance for the drag and drop operation.
+            dragDelta.x = canonViewer.getLayoutX()+canonViewer.getFitWidth()/2;
+            dragDelta.y = canonViewer.getLayoutY()+canonViewer.getFitHeight()/2;
+            n.setCursor(Cursor.MOVE);
+        });
+        n.setOnMouseReleased(e -> n.setCursor(Cursor.HAND));
+        n.setOnMouseDragged(mouseEvent-> {
+            double changex = mouseEvent.getSceneX()-dragDelta.x;
+            double changey = mouseEvent.getSceneY()-dragDelta.y;
+            double angle = Math.toDegrees(Math.atan(changey / changex));
+            if(changex<0)
+                angle+=180;
+            m.setRotate(angle);
+            if(angle<90)
+                angle = -angle;
+            else
+                angle = 360-angle;
+            angleField.setText(""+angle);
+        });
+        n.setOnMouseEntered(e -> n.setCursor(Cursor.HAND));
     }
 
     private static final class Delta{
